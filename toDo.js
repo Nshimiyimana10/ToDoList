847e84d0ed41a52aa2928d15c8183deafc092019
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-10 12:07:17 -0700

    added js content

--- toDo.js
+++ toDo.js
@@ -1,2 +1,8 @@
-const taskInput = document.getElementById("task-input");
+const taskInput = document.getElementById("task-input").value;
 const button = document.getElementById("add-task-button");
+
+button.addEventListener('click', getInputValue)
+
+function getInputValue(){
+    console.log('button clicked');
+}
